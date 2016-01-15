65c63f784857e7b8e2c8d3731d8360fa6d193b03
Antonis Tsakiridis <antonis.tsakiridis@telestax.com>
2016-01-15 05:49:24 +0200

    Fixed #26: Make sure build script works

--- build.sh
+++ build.sh
@@ -5,8 +5,9 @@
 rm -R build
 mkdir build
 
-cat ./src/*.js > ./build/restcomm-webrtc-sdk.debug.js
+#cat ./src/*.js > ./build/restcomm-webrtc-sdk.debug.js
+cat ./src/*.js > ./build/restcomm-webrtc-sdk.js
 
-grep -v "logger.debug" ./build/restcomm-webrtc-sdk.debug.js  > ./build/restcomm-webrtc-sdk.js
+#grep -v "logger.debug" ./build/restcomm-webrtc-sdk.debug.js  > ./build/restcomm-webrtc-sdk.js
 
 java -jar yuicompressor-2.4.7.jar ./build/restcomm-webrtc-sdk.js -o ./build/restcomm-webrtc-sdk.min.js
