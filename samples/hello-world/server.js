ae0d0e5ffcdc04edf2c821868774f682f0dc322e
Antonis Tsakiridis <antonis.tsakiridis@telestax.com>
2016-03-01 14:46:01 +0200

    Fixed #31: Allow a sample application to register with Restcomm without user intervention

--- samples/hello-world/server.js
+++ samples/hello-world/server.js
@@ -1,13 +1,8 @@
 var static = require('node-static');
-var http = require('https');
+var http = require('http');
 var fs = require('fs');
 
-var options = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem')
-};
-
 var file = new(static.Server)();
-var app = http.createServer(options, function (req, res) {
+var app = http.createServer(function (req, res) {
   file.serve(req, res);
-}).listen(8443);
+}).listen(7080);
