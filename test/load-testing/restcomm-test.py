2b9a5f33493d23a4cd1ac86a0dd27bfec5baaad7
Antonis Tsakiridis <antonis.tsakiridis@telestax.com>
2016-03-05 18:19:42 +0200

    Fixed #39: Automate Restcomm number Provisioning and make it point to the provided external service url in progress.  Fixed #40: Allow all Restcomm provisioning to happen over https and ignore server certificatio issues (i.e. unsigned).  Fixed #41: Properly urlencode all arguments to GET/POST requests.  Fixed #42: Allow username username prefix and password to be configurable.

--- test/load-testing/restcomm-test.py
+++ test/load-testing/restcomm-test.py
@@ -3,12 +3,14 @@
 # Load testing Restcomm Media Server
 #
 # Example invocations:
-# $ sudo ./restcomm-test.py --client-count 10 --client-url http://127.0.0.1:10510/webrtc-client.html --client-user-prefix user --client-password 1234 --account-sid ACae6e420f425248d6a26948c17a9e2acf --auth-token 0d01c95aac798602579fe08fc2461036 --http-protocol http --transport 127.0.0.1:8080
+# - Insecure invocation:
+# $ ./restcomm-test.py --client-count 1 --client-url http://127.0.0.1:10510/webrtc-client.html --client-register-ws-url ws://127.0.0.1:5082 --client-register-domain 127.0.0.1 --client-username-prefix user --client-password 1234 --restcomm-account-sid ACae6e420f425248d6a26948c17a9e2acf --restcomm-auth-token 0d01c95aac798602579fe08fc2461036  --restcomm-base-url http://127.0.0.1:8080 --restcomm-phone-number "+5556" --restcomm-external-service-url http://127.0.0.1:10512/rcml
+# - Secure invocation:
+# $ restcomm-test.py --client-count 1 --client-url https://127.0.0.1:10511/webrtc-client.html --client-register-ws-url wss://54.146.130.52:5083 --client-register-domain 54.146.130.52 --client-username-prefix user --client-password 1234 --restcomm-account-sid ACae6e420f425248d6a26948c17a9e2acf --restcomm-auth-token 0d01c95aac798602579fe08fc2461036  --restcomm-base-url https://54.146.130.52 --restcomm-phone-number "+5556" --restcomm-external-service-url http://127.0.0.1:10512/rcml
+#
 # TODOs:
 #
-# - Provision the Restcomm Number as well and make it point to our external service via REST
-# - Fix the unprovisioning functionality also remove the Restcomm Clients and Restcomm Number (see above)
-# - Allow Restcomm provisioning occur over https. Right http is hard coded
+# - Fix the unprovisioning functionality also remove the Restcomm Clients and Restcomm Number
 #
 
 import argparse
@@ -17,6 +19,8 @@ import json
 import time
 import subprocess 
 import os 
+import re
+import urllib
 
 # Notice that we are using the dummy module which is implemented with threads,
 # not multiple processes, as processes might be overkill in our situation (in
@@ -39,6 +43,7 @@ import selenium.common.exceptions
 
 # Globals
 TAG = '[restcomm-test] '
+# Keep the nodejs process in a global var so that we can reference it after the tests are over to shut it down
 httpProcess = None
 
 def threadFunction(dictionary): 
@@ -51,8 +56,8 @@ def threadFunction(dictionary):
 	caps = DesiredCapabilities.CHROME
 	#caps['loggingPrefs'] = {'browser': 'ALL', 'client': 'ALL', 'driver': 'ALL', 'performance': 'ALL', 'server': 'ALL'}
 	caps['loggingPrefs'] = { 'browser':'ALL' }
-	#driver = webdriver.Chrome(chrome_options = chromeOptions, desired_capabilities = caps, service_args = ["--verbose", "--log-path=chrome.log"]);
-	driver = webdriver.Chrome(chrome_options = chromeOptions, desired_capabilities = caps);
+	#driver = webdriver.Chrome(chrome_options = chromeOptions, desired_capabilities = caps, service_args = ["--verbose", "--log-path=chrome.log"])
+	driver = webdriver.Chrome(chrome_options = chromeOptions, desired_capabilities = caps)
 
 	# navigate to web page
 	driver.get(dictionary['url'])
@@ -62,12 +67,10 @@ def threadFunction(dictionary):
 	try:
 		#WebDriverWait(driver, 30).until(expected_conditions.text_to_be_present_in_element((By.ID,'log'), 'Connection ended'))
 		# this is actually a hack to keep the browser open for n seconds. Putting the thread to sleep doesn't work and so far I haven't found a nice way to do that in Selenium
-		WebDriverWait(driver, 60).until(expected_conditions.text_to_be_present_in_element((By.ID,'log'), 'Non existing text'))
+		WebDriverWait(driver, 660).until(expected_conditions.text_to_be_present_in_element((By.ID,'log'), 'Non existing text'))
 	except selenium.common.exceptions.TimeoutException as ex:
 		print TAG + '#' + str(dictionary['id']) + ' Test timed out'
 		
-	#wait.until(expected_conditions.title_contains('RestComm'))
-
 	# print messages
 	print TAG + '#' + str(dictionary['id']) + ' Saving the logs'
 	logBuffer = ''
@@ -79,31 +82,80 @@ def threadFunction(dictionary):
 	logFile.write(logBuffer)
 	logFile.close()
 
-	#assert "Python" in driver.title
-	#elem = driver.find_element_by_name("q")
-	#elem.send_keys("pycon")
-	#elem.send_keys(Keys.RETURN)
-	#assert "No results found." not in driver.page_source
-
 	print TAG + '#' + str(dictionary['id']) + ' Closing Driver'
 	driver.close()
+
+
+# take a url and break it in protocol and transport counterparts
+def breakUrl(url):
+	matches = re.search('(^.*?:\/\/)(.*$)', url)
+	protocol = matches.group(1)
+	transport = matches.group(2)
+	return protocol, transport
+
+# Return the account base Restcomm URL, like http://ACae6e420f425248d6a26948c17a9e2acf:0d01c95aac798602579fe08fc2461036@127.0.0.1:8080/restcomm/2012-04-24/Accounts/ACae6e420f425248d6a26948c17a9e2acf,
+# from base URL (i.e. http://127.0.0.1:8080), account sid and auth token
+def restBaseUrlFromCounterparts(accountSid, authToken, restcommUrl):
+	# Need to break URL in protocol and transport parts so that we can put account sid and auth token in between 
+	matches = re.search('(^.*?:\/\/)(.*$)', restcommUrl)
+	#protocol = matches.group(1)
+	#transport = matches.group(2)
+	protocol, transport = breakUrl(restcommUrl)
+	return protocol + accountSid + ':' + authToken + '@' + transport + '/restcomm/2012-04-24/Accounts/' + accountSid
+
+# curl will break if we target an https server that has self signed certificate. Let's always use -k (avoid checks for cert) when targeting https
+def curlSecureOptionsIfApplicable(restcommUrl):
+	protocol, transport = breakUrl(restcommUrl)
+	if protocol == 'https://':
+		return '-k'
+	else:
+		return ''
+
+# Provision Restcomm Number for external service via REST call
+def provisionPhoneNumber(phoneNumber, externalServiceUrl, accountSid, authToken, restcommUrl): 
+	print TAG + "Provisioning phone number " + phoneNumber + ' and linking it with Voice URL: ' + externalServiceUrl
+	devnullFile = open(os.devnull, 'w')
+	# Need to break URL in protocol and transport parts so that we can put account sid and auth token in between 
+	matches = re.search('(^.*?:\/\/)(.*$)', restcommUrl)
+	protocol = matches.group(1)
+	transport = matches.group(2)
+	postData = {
+		'PhoneNumber': phoneNumber,
+		'VoiceUrl': externalServiceUrl,
+		'FriendlyName': 'Load Testing App',
+		'isSIP' : 'true',
+	}
+	#cmd = 'curl -X POST ' + restBaseUrlFromCounterparts(accountSid, authToken, restcommUrl) + '/IncomingPhoneNumbers.json -d PhoneNumber=' + phoneNumber + ' -d VoiceUrl=' + externalServiceUrl + ' -d FriendlyName=LoadTestingApp -d isSIP=true'
+	cmd = 'curl ' + curlSecureOptionsIfApplicable(restcommUrl) + ' -X POST ' + restBaseUrlFromCounterparts(accountSid, authToken, restcommUrl) + '/IncomingPhoneNumbers.json -d ' + urllib.urlencode(postData)
+	print TAG + cmd 
+	subprocess.call(cmd.split(), stdout = devnullFile, stderr = devnullFile)
+
 
 # Provision Restcomm Clients via REST call
 # count: number of Clients to provision
 # accountSid: Restcomm accountSid, like: ACae6e420f425248d6a26948c17a9e2acf
 # authToken: Restcomm authToken, like: 0a01c34aac72a432579fe08fc2461036 
-# transport: Restcomm transport, like: 127.0.0.1:8080
-def provisionClients(count, accountSid, authToken, transport): 
-	print TAG + "Provisioning " + str(count) + " Restcomm Clients";
+# restcommUrl: Restcomm URL, like: http://127.0.0.1:8080
+def provisionClients(count, accountSid, authToken, restcommUrl, usernamePrefix, password): 
+	print TAG + "Provisioning " + str(count) + " Restcomm Clients"
 	devnullFile = open(os.devnull, 'w')
+	# Need to break URL in protocol and transport parts so that we can put account sid and auth token in between 
+	matches = re.search('(^.*?:\/\/)(.*$)', restcommUrl)
+	protocol = matches.group(1)
+	transport = matches.group(2)
 	for i in range(1, count + 1):
-		cmd = 'curl -X POST http://' + accountSid + ':' + authToken + '@' + transport + '/restcomm/2012-04-24/Accounts/' + accountSid + '/Clients.json -d Login=user' + str(i) + ' -d Password=1234';
-		#system(cmd);
+		postData = {
+			'Login': usernamePrefix + str(i),
+			'Password': password,
+		}
+		#cmd = 'curl -X POST ' + restBaseUrlFromCounterparts(accountSid, authToken, restcommUrl) + '/Clients.json -d Login=user' + str(i) + ' -d Password=1234'
+		cmd = 'curl ' + curlSecureOptionsIfApplicable(restcommUrl) + ' -X POST ' + restBaseUrlFromCounterparts(accountSid, authToken, restcommUrl) + '/Clients.json -d ' + urllib.urlencode(postData)
+		#system(cmd)
 		print TAG + cmd 
 		subprocess.call(cmd.split(), stdout = devnullFile, stderr = devnullFile)
 
-def startServer(count): 
-	print TAG + 'Starting unified node http server to handle both http/https request for the webrtc-client web page, and RCML REST requests from Restcomm';
+def startServer(count, clientUrl, externalServiceUrl): 
+	print TAG + 'Starting unified node http server to handle both http/https request for the webrtc-client web page, and RCML REST requests from Restcomm'
 
 	# Make a copy of the current environment
 	envDictionary = dict(os.environ)   
@@ -118,12 +170,11 @@ def startServer(count):
 	print TAG + 'PID for http server: ' + str(httpProcess.pid)
 
 # TODO: Not finished yet
-def unprovisionClients(count, accountSid, authToken, transport): 
-	print TAG + "(Not implemented yet) Unprovisioning " + str(count) + " Restcomm Clients";
+def unprovisionClients(count, accountSid, authToken, restcommUrl): 
+	print TAG + "(Not implemented yet) Unprovisioning " + str(count) + " Restcomm Clients"
 	#for i in range(1, count + 1):
-	#	cmd = 'curl -X DELETE http://' + accountSid + ':' + authToken + '@' + transport + '/restcomm/2012-04-24/Accounts/' + accountSid + '/Clients.json -d Login=user' + str(i) + ' -d Password=1234';
-	#	#print TAG + cmd 
-	#	subprocess.call(cmd.split())
+	#	cmd = 'curl ' + curlSecureOptionsIfApplicable(restcommUrl) + ' -X DELETE http://' + accountSid + ':' + authToken + '@' + transport + '/restcomm/2012-04-24/Accounts/' + accountSid + '/Clients.json -d Login=user' + str(i) + ' -d Password=1234'
+	#	...
 
 def stopServer(): 
 	if httpProcess:
@@ -134,16 +185,19 @@ def globalSetup(dictionary):
 	print TAG + "Setting up tests"
 
 	# Provision Restcomm with the needed Clients
-	provisionClients(dictionary['count'], dictionary['account-sid'], dictionary['auth-token'], dictionary['transport'])
+	provisionPhoneNumber(dictionary['phone-number'], dictionary['external-service-url'], dictionary['account-sid'], dictionary['auth-token'], dictionary['restcomm-base-url'])
+
+	# Provision Restcomm with the needed Clients
+	provisionClients(dictionary['count'], dictionary['account-sid'], dictionary['auth-token'], dictionary['restcomm-base-url'], dictionary['username-prefix'], dictionary['password'])
 
 	# Start the unified server script to serve both RCML (REST) and html page for webrtc clients to connect to
-	startServer(dictionary['count'])
+	startServer(dictionary['count'], dictionary['client-url'], dictionary['external-service-url'])
 
 def globalTeardown(dictionary): 
 	print TAG + "Tearing down tests"
 
 	# Provision Restcomm with the needed Clients
-	unprovisionClients(dictionary['count'], dictionary['account-sid'], dictionary['auth-token'], dictionary['transport'])
+	unprovisionClients(dictionary['count'], dictionary['account-sid'], dictionary['auth-token'], dictionary['restcomm-base-url'])
 
 	# Start the unified server script to serve both RCML (REST) and html page for webrtc clients to connect to
 	stopServer()
@@ -152,35 +206,47 @@ def globalTeardown(dictionary):
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--client-count', dest = 'count', default = 10, type = int, help = 'Count of Webrtc clients spawned for the test')
-parser.add_argument('-u', '--client-url', dest = 'url', default = 'http://127.0.0.1:10510/webrtc-client.html', help = 'Webrtc clients target URL, like \'http://127.0.0.1:10510/webrtc-client.html\'')
-parser.add_argument('-w', '--client-register-ws-url', dest = 'registerWsUrl', default = 'ws://127.0.0.1:5082', help = 'Webrtc clients target websocket URL for registering, like \'ws://127.0.0.1:5082\'')
-parser.add_argument('-d', '--client-register-domain', dest = 'registerDomain', default = '127.0.0.1', help = 'Webrtc clients domain for registering, like \'127.0.0.1\'')
-#parser.add_argument('-e', '--external-service', dest = 'externalService', default = 'http://127.0.0.1:10512/rcml', help = 'External service for Restcomm to get RCML, like \'http://127.0.0.1:10512/rcml\'')
-parser.add_argument('-x', '--client-user-prefix', dest = 'userPrefix', default = 'user', help = 'User prefix for the clients, like \'user\'')
-parser.add_argument('-p', '--client-password', dest = 'password', default = '1234', help = 'Password for the clients, like \'1234\'')
-parser.add_argument('-s', '--account-sid', dest = 'accountSid', required = True, help = 'Restcomm accound Sid, like \'ACae6e420f425248d6a26948c17a9e2acf\'')
-parser.add_argument('-a', '--auth-token', dest = 'authToken', required = True, help = 'Restcomm auth token, like \'0a01c34aac72a432579fe08fc2461036\'')
-parser.add_argument('-r', '--http-protocol', dest = 'httpProtocol', default = 'http', help = 'Restcomm http protocol: \'http\' (default) or \'https\'')
-parser.add_argument('-t', '--transport', dest = 'transport', default = '127.0.0.1:8080', help = 'Restcomm transport, like \'127.0.0.1:8080\'')
+parser.add_argument('--client-url', dest = 'clientUrl', default = 'http://127.0.0.1:10510/webrtc-client.html', help = 'Webrtc clients target URL, like \'http://127.0.0.1:10510/webrtc-client.html\'')
+parser.add_argument('--client-register-ws-url', dest = 'registerWsUrl', default = 'ws://127.0.0.1:5082', help = 'Webrtc clients target websocket URL for registering, like \'ws://127.0.0.1:5082\'')
+parser.add_argument('--client-register-domain', dest = 'registerDomain', default = '127.0.0.1', help = 'Webrtc clients domain for registering, like \'127.0.0.1\'')
+parser.add_argument('--client-username-prefix', dest = 'usernamePrefix', default = 'user', help = 'User prefix for the clients, like \'user\'')
+parser.add_argument('--client-password', dest = 'password', default = '1234', help = 'Password for the clients, like \'1234\'')
+parser.add_argument('--restcomm-base-url', dest = 'restcommBaseUrl', default = 'http://127.0.0.1:8080', help = 'Restcomm instance base URL, like \'http://127.0.0.1:8080\'')
+parser.add_argument('--restcomm-account-sid', dest = 'accountSid', required = True, help = 'Restcomm accound Sid, like \'ACae6e420f425248d6a26948c17a9e2acf\'')
+parser.add_argument('--restcomm-auth-token', dest = 'authToken', required = True, help = 'Restcomm auth token, like \'0a01c34aac72a432579fe08fc2461036\'')
+parser.add_argument('--restcomm-phone-number', dest = 'phoneNumber', default = '+5556', help = 'Restcomm phone number to provision and link with external service, like \'+5556\'')
+parser.add_argument('--restcomm-external-service-url', dest = 'externalServiceUrl', default = 'http://127.0.0.1:10512/rcml', help = 'External service URL for Restcomm to get RCML from, like \'http://127.0.0.1:10512/rcml\'')
 
 args = parser.parse_args()
 
-print TAG + 'Using Webrtc Client count: ' + str(args.count) + ', targeting URL: ' + args.url + ', using password: ' + args.password
-print TAG + 'Using Restcomm instance at: ' + args.transport + ' over ' + args.httpProtocol + ', with Account Sid: ' + args.accountSid + ' and Auth Token: ' + args.authToken
+print TAG + 'Webrtc clients settings: \n\tcount: ' + str(args.count) + '\n\ttarget URL: ' + args.clientUrl + '\n\tregister websocket url: ' + args.registerWsUrl + '\n\tregister domain: ' + args.registerDomain + '\n\tusername prefix: ' + args.usernamePrefix + '\n\tpassword: ' + args.password
+print TAG + 'Restcomm instance settings: \n\tbase URL: ' + args.restcommBaseUrl + '\n\taccount sid: ' + args.accountSid + '\n\tauth token: ' + args.authToken + '\n\tphone number: ' + args.phoneNumber + '\n\texternal service URL: ' + args.externalServiceUrl
 
-# Populate a list with ids and URLs for each client thread that will be spawned
+# Populate a list with browser thread ids and URLs for each client thread that will be spawned
 urls = list()
 for i in range(1, args.count + 1):
+	getData = {
+		'username': args.usernamePrefix + str(i),
+		'password': args.password,
+		'register-ws-url': args.registerWsUrl,
+		'register-domain' : args.registerDomain,
+	}
 	urls.append({ 
 		'id': i, 
-		'url' : args.url + '?username=' + args.userPrefix + str(i) + '&password=' + args.password + '&register-ws-url=' + args.registerWsUrl + '&register-domain=' + args.registerDomain,
+		#'url' : args.clientUrl + '?username=' + args.usernamePrefix + str(i) + '&password=' + args.password + '&register-ws-url=' + args.registerWsUrl + '&register-domain=' + args.registerDomain,
+		'url' : args.clientUrl + '?' + urllib.urlencode(getData)
 	})
 
 globalSetup({ 
 	'count': args.count, 
+	'client-url': args.clientUrl, 
+	'username-prefix': args.usernamePrefix,
+	'password': args.password,
 	'account-sid': args.accountSid, 
 	'auth-token': args.authToken, 
-	'transport': args.transport 
+	'restcomm-base-url': args.restcommBaseUrl,
+	'phone-number': args.phoneNumber, 
+	'external-service-url': args.externalServiceUrl
 })
 
 #sys.exit(0)
@@ -202,7 +268,11 @@ else:
 
 globalTeardown({ 
 	'count': args.count, 
+	'username': args.password,
+	'password': args.password,
 	'account-sid': args.accountSid, 
 	'auth-token': args.authToken, 
-	'transport': args.transport 
+	'restcomm-base-url': args.restcommBaseUrl,
+	'phone-number': args.phoneNumber, 
+	'external-service-url': args.externalServiceUrl
 })
